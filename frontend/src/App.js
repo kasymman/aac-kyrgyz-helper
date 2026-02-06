b54c7ad04b72ab907989eb6ca420a264a7d491d5
kasymman <77896083+kasymman@users.noreply.github.com>
2026-02-07 00:04:54 +0600

    Create main React App component

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div className="App">
+            <h1>Welcome to the Kyrgyz Helper!</h1>
+            <p>This is a React application to assist with Kyrgyz language resources.</p>
+        </div>
+    );
+}
+
+export default App;
